37a7e70aa730bbba34028eada7049ed935454f92
KimMalsu <jsm4451@naver.com>
2022-04-30 21:42:16 +0900

    1

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { UserDataEntity } from './entities/userData.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, UserDataEntity])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],

61bc64be8b9d350a1ce15ad182c62fcd43f1f558
KimMalsu <jsm4451@naver.com>
2022-06-06 01:43:00 +0900

    fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -126,6 +126,7 @@ export class UsersService {
       .createQueryBuilder(UserRecordEntity, 'record')
       .leftJoinAndSelect('record.game', 'game')
       .orderBy('game.gamename', 'ASC')
+      .andWhere('game.gamename not like :game1', { game1: `${'CIST'}%` })
       .addOrderBy('difficulty', 'ASC')
       .getMany();
 
